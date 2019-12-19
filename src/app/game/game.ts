ff256b27d6854bbe5efcae5ef2537ddafa0e2ea3
Kamule <benjamin.herbomez@gmail.com>
2019-12-19 16:36:02 +0100

    Update game.ts

--- src/app/game/game.ts
+++ src/app/game/game.ts
@@ -1,17 +1,19 @@
-import {Engine, Color} from 'excalibur';
+import {Engine, Color, DisplayMode} from 'excalibur';
 import { Subject  } from 'rxjs';
 
 import { HomeScene } from './scenes/index';
+import { Menu } from '../menu/menu.service';
 
 export class Game extends Engine{
 
-  public actionRequired:Subject <string> = new Subject<string>();
+  public actionRequired:Subject <Menu> = new Subject<Menu>();
 
   constructor(canvas: string){
     super({
         canvasElementId: canvas,
         width: 1000,
-        height: 1000            
+        height: 1000,
+        displayMode: DisplayMode.Container,  
     })
     this.backgroundColor = Color.fromHex("0A0863");
 
